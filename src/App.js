cdfc2dd47435c94b62275375a748c2ce77dbfd25
Opeyemi Oyeleke <topfemo@gmail.com>
2019-06-26 18:51:27 +0100

    Created Components

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">column number one</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home" />
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <h3>Hello ope</h3>
+    </React.Fragment>
   );
 }
 
